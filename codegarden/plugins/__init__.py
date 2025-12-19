0e5fb5869272e1fb0680abf5ee9cc92d87caa9d8
theowebmigdalened <theowebmigdalened@gmail.com>
2025-12-19 14:42:50 +0200

    Deleted snippet 'strip_yaml_frontmatter' from codegarden/plugins/__init__.py

--- codegarden/plugins/__init__.py
+++ codegarden/plugins/__init__.py
@@ -10,15 +10,7 @@ def top_tags(index: dict, n: int = 10):
 # --- endsnippet ---
 
 
-# --- snippet: strip_yaml_frontmatter ---
-def strip_yaml_frontmatter(text: str) -> str:
-    """Убирает YAML фронтматтер из начала Markdown."""
-    if text.startswith("---"):
-        end = text.find("\n---", 3)
-        if end != -1:
-            return text[end+4:]
-    return text
-# --- endsnippet ---
+
 
 
 # autosave 2025-11-21T14:15:19.466363
