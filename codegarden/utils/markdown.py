2636592228debb14e8fef443aec8f91b0e3ac3f8
theowebmigdalened <theowebmigdalened@gmail.com>
2025-12-22 21:58:09 +0200

    Touched codegarden/utils/markdown.py (no snippet change)

--- codegarden/utils/markdown.py
+++ codegarden/utils/markdown.py
@@ -8,3 +8,5 @@ HEADING_RX = re.compile(r"^(#{1,6})\s+(.*)$", re.MULTILINE)
 def extract_headings(md_text: str):
     return [(m.group(1), m.group(2).strip()) for m in HEADING_RX.finditer(md_text or "")]
 # tweak 2025-10-10T12:51:32.415950
+
+# autosave 2025-12-22T19:58:09.373780
