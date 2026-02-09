82ce941c9ed5e9662fa26e39a5ee01f0cc3565b2
theowebmigdalened <theowebmigdalened@gmail.com>
2026-02-09 17:41:41 +0200

    Added snippet 'top_tags' into codegarden/utils/__init__.py

--- codegarden/utils/__init__.py
+++ codegarden/utils/__init__.py
@@ -4,3 +4,14 @@
 
 # autosave 2025-12-19T18:44:13.497906
 # tweak 2026-01-19T10:20:53.146164
+
+# --- snippet: top_tags ---
+def top_tags(index: dict, n: int = 10):
+    """Топ N тегов по частоте."""
+    from collections import Counter
+    c = Counter()
+    for meta in index.values():
+        c.update(meta.get("tags", []))
+    return c.most_common(n)
+# --- endsnippet ---
+
